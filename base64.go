24a10e8f75f038b87aac81f7a2ca1ef083acfd52
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2018-12-27 14:11:39 +0100

    base64 documentation

--- base64.go
+++ base64.go
@@ -16,23 +16,27 @@ package gopherbounce
 
 import "encoding/base64"
 
+// Base64Encoding is the internal reprensetation of a base64 encoding.
 type Base64Encoding struct {
 	Alphabet string
 	Encoding *base64.Encoding
 }
 
-func NewBase64Encoding(alphabet string, removePadding bool) *Base64Encoding {
+// NewBase64Encoding returns a new Base64Encoding.
+func NewBase64Encoding(alphabet string) *Base64Encoding {
 	return &Base64Encoding{
 		Alphabet: alphabet,
 		Encoding: base64.NewEncoding(alphabet),
 	}
 }
 
+// SetAlphabet sets the encoding alphabet to a new alphabet.
 func (enc *Base64Encoding) SetAlphabet(alphabet string) {
 	enc.Alphabet = alphabet
 	enc.Encoding = base64.NewEncoding(alphabet)
 }
 
+// Base64Encode encodes the source to base64 using the alphabet.
 func (enc *Base64Encoding) Base64Encode(src []byte) []byte {
 	n := enc.Encoding.EncodedLen(len(src))
 	dst := make([]byte, n)
@@ -40,6 +44,7 @@ func (enc *Base64Encoding) Base64Encode(src []byte) []byte {
 	return dst[:n]
 }
 
+// Base64Decode decodes the source using the alphabet.
 func (enc *Base64Encoding) Base64Decode(src []byte) ([]byte, error) {
 	dst := make([]byte, enc.Encoding.DecodedLen(len(src)))
 	n, err := enc.Encoding.Decode(dst, src)
@@ -49,14 +54,18 @@ func (enc *Base64Encoding) Base64Decode(src []byte) ([]byte, error) {
 	return dst[:n], nil
 }
 
+// Defaultalphabet is the alphabet used by most algorithms.
 const Defaultalphabet = "./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
-var DefaultEncoding = NewBase64Encoding(Defaultalphabet, false)
+// DefaultEncoding is the internal encoding based on Defaultalphabet.
+var DefaultEncoding = NewBase64Encoding(Defaultalphabet,)
 
+// Base64Encode encodes the source to base64 using the default encoding.
 func Base64Encode(src []byte) []byte {
 	return DefaultEncoding.Base64Encode(src)
 }
 
+// Base64Decode decodes the source using the encoding.
 func Base64Decode(src []byte) ([]byte, error) {
 	return DefaultEncoding.Base64Decode(src)
 }
