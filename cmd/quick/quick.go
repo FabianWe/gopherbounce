43cd791679b5f1234e7969469ec8880daff1dece
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2018-12-27 14:41:14 +0100

    quickstart example

---
+++ cmd/quick/quick.go
@@ -0,0 +1,43 @@
+// Copyright 2018 Fabian Wenzelmann
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+  "fmt"
+
+  "github.com/FabianWe/gopherbounce"
+)
+
+func main() {
+  password := "foobar"
+  hashed, hashErr := gopherbounce.DefaultHasher.Generate(password)
+  if hashErr != nil {
+    panic(hashErr)
+  }
+  fmt.Println("Hashed password:", string(hashed))
+  validator := gopherbounce.GuessValidatorFunc(hashed)
+  okay := validator("foobar")
+  if okay == nil {
+    fmt.Println("Password correct")
+  } else {
+    fmt.Println("Password wrong")
+  }
+  okay = validator("eggs")
+  if okay == nil {
+    fmt.Println("Password correct")
+  } else {
+    fmt.Println("Password wrong")
+  }
+}
