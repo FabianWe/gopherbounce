4926d38b655f766a1ee226fb687f5e79299a618d
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2019-01-07 04:44:24 +0100

    copyright note update

--- cmd/quick/quick.go
+++ cmd/quick/quick.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Fabian Wenzelmann
+// Copyright 2018, 2019 Fabian Wenzelmann
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- cmd/tune/tune.go
+++ cmd/tune/tune.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Fabian Wenzelmann
+// Copyright 2018, 2019 Fabian Wenzelmann
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,85 +15,85 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
-  "time"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 
-  "github.com/FabianWe/gopherbounce"
+	"github.com/FabianWe/gopherbounce"
 )
 
 func main() {
 
-  if len(os.Args) != 3 {
-    fmt.Println("Invalid syntax")
-    fmt.Printf("Usage: %s alg t\n", os.Args[0])
-    fmt.Printf("Example: %s bcrypt 241ms\n", os.Args[0])
-    os.Exit(1)
-  }
+	if len(os.Args) != 3 {
+		fmt.Println("Invalid syntax")
+		fmt.Printf("Usage: %s alg t\n", os.Args[0])
+		fmt.Printf("Example: %s bcrypt 241ms\n", os.Args[0])
+		os.Exit(1)
+	}
 
-  duration, parseErr := time.ParseDuration(os.Args[2])
-  if parseErr != nil {
-    fmt.Println("Invalid duration argument:", parseErr.Error())
-    os.Exit(1)
-  }
+	duration, parseErr := time.ParseDuration(os.Args[2])
+	if parseErr != nil {
+		fmt.Println("Invalid duration argument:", parseErr.Error())
+		os.Exit(1)
+	}
 
-  switch strings.ToLower(os.Args[1]) {
-  case "bcrypt":
-    tuneBcrypt(duration)
-  case "scrypt":
-    tuneScrypt(duration)
-  case "argon2i":
-    tuneArgon2i(duration)
-  case "argon2id":
-    tuneArgon2id(duration)
-  default:
-    fmt.Printf("Invalid algorithm \"%s\"\n", os.Args[1])
-    fmt.Println("Valid algorithms are")
-    fmt.Println("  * bcrypt")
-    fmt.Println("  * scrypt")
-    fmt.Println("  * argon2i")
-    fmt.Println("  * argon2id")
-    os.Exit(1)
-  }
+	switch strings.ToLower(os.Args[1]) {
+	case "bcrypt":
+		tuneBcrypt(duration)
+	case "scrypt":
+		tuneScrypt(duration)
+	case "argon2i":
+		tuneArgon2i(duration)
+	case "argon2id":
+		tuneArgon2id(duration)
+	default:
+		fmt.Printf("Invalid algorithm \"%s\"\n", os.Args[1])
+		fmt.Println("Valid algorithms are")
+		fmt.Println("  * bcrypt")
+		fmt.Println("  * scrypt")
+		fmt.Println("  * argon2i")
+		fmt.Println("  * argon2id")
+		os.Exit(1)
+	}
 }
 
 func tuneBcrypt(duration time.Duration) {
-  conf, avg, tuneErr := gopherbounce.TuneBcrypt(nil, duration)
-  if tuneErr != nil {
-    fmt.Println("Error tuning bcrypt:", tuneErr.Error())
-    os.Exit(1)
-  }
-  fmt.Println("bcrypt tuning computed the following config:", conf)
-  fmt.Println("Average with this cost is", avg)
+	conf, avg, tuneErr := gopherbounce.TuneBcrypt(nil, duration)
+	if tuneErr != nil {
+		fmt.Println("Error tuning bcrypt:", tuneErr.Error())
+		os.Exit(1)
+	}
+	fmt.Println("bcrypt tuning computed the following config:", conf)
+	fmt.Println("Average with this cost is", avg)
 }
 
 func tuneScrypt(duration time.Duration) {
-  conf, avg, tuneErr := gopherbounce.TuneScrypt(nil, duration)
-  if tuneErr != nil {
-    fmt.Println("Error tuning scrypt:", tuneErr.Error())
-    os.Exit(1)
-  }
-  fmt.Println("scrypt tuning computed the following config:", conf)
-  fmt.Println("Average with this cost is", avg)
+	conf, avg, tuneErr := gopherbounce.TuneScrypt(nil, duration)
+	if tuneErr != nil {
+		fmt.Println("Error tuning scrypt:", tuneErr.Error())
+		os.Exit(1)
+	}
+	fmt.Println("scrypt tuning computed the following config:", conf)
+	fmt.Println("Average with this cost is", avg)
 }
 
 func tuneArgon2i(duration time.Duration) {
-  conf, avg, tuneErr := gopherbounce.TuneArgon2i(nil, duration)
-  if tuneErr != nil {
-    fmt.Println("Error tuning argon2i:", tuneErr.Error())
-    os.Exit(1)
-  }
-  fmt.Println("argon2i tuning computed the following config:", conf)
-  fmt.Println("Average with this cost is", avg)
+	conf, avg, tuneErr := gopherbounce.TuneArgon2i(nil, duration)
+	if tuneErr != nil {
+		fmt.Println("Error tuning argon2i:", tuneErr.Error())
+		os.Exit(1)
+	}
+	fmt.Println("argon2i tuning computed the following config:", conf)
+	fmt.Println("Average with this cost is", avg)
 }
 
 func tuneArgon2id(duration time.Duration) {
-  conf, avg, tuneErr := gopherbounce.TuneArgon2id(nil, duration)
-  if tuneErr != nil {
-    fmt.Println("Error tuning argon2id:", tuneErr.Error())
-    os.Exit(1)
-  }
-  fmt.Println("argon2id tuning computed the following config:", conf)
-  fmt.Println("Average with this cost is", avg)
+	conf, avg, tuneErr := gopherbounce.TuneArgon2id(nil, duration)
+	if tuneErr != nil {
+		fmt.Println("Error tuning argon2id:", tuneErr.Error())
+		os.Exit(1)
+	}
+	fmt.Println("argon2id tuning computed the following config:", conf)
+	fmt.Println("Average with this cost is", avg)
 }
